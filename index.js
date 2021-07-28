dcac0fe11ac34c736dc96cb79b27d600d8e0690a
lavish.gambhir <lavish.gambhir@cyfuture.com>
2021-07-28 11:53:53 +0530

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(process.env.PORT || 3000, function(){
+         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+       });
