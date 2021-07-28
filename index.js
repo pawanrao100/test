8e8a1562ca83aec5c3d0750b9a22bb6c651ab536
lavish.gambhir <lavish.gambhir@cyfuture.com>
2021-07-28 13:19:17 +0530

    according to rule

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+const port = process.env.PORT || 8000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+}).listen(port, ()=>{
+  console.log("pawan rao node js ");
        });
