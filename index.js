c75ad4881488cb1d74704ad9e5d6f23e1d3147a4
lavish.gambhir <lavish.gambhir@cyfuture.com>
2021-07-28 11:56:04 +0530

    index changes

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }).listen(process.env.PORT || 3000, function(){
-         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+ console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
        });
